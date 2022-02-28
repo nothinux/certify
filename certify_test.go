36790115ff89a74d1754ccb954efd43bc22acc51
nothinux <nothinux@gmail.com>
2022-02-28 12:04:26 +0700

    add test

---
+++ certify_test.go
@@ -0,0 +1,69 @@
+package certify
+
+import (
+	"crypto/x509/pkix"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestGetCertificate(t *testing.T) {
+	pkey, err := GetPrivateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	template := Certificate{
+		Subject: pkix.Name{
+			Organization: []string{"certify"},
+			CommonName:   "certify",
+		},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(24 * time.Hour),
+		IsCA:      true,
+		DNSNames:  []string{"github.com"},
+		IPAddress: []net.IP{
+			net.ParseIP("127.0.0.1"),
+		},
+	}
+
+	cert, err := template.GetCertificate(pkey.PrivateKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Test created certificate match with given information", func(t *testing.T) {
+		c, err := ParseCertificate([]byte(cert.String()))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if c.Subject.CommonName != "certify" {
+			t.Fatalf("got %v, want certify", c.Subject.CommonName)
+		}
+
+		if c.Subject.Organization[0] != "certify" {
+			t.Fatalf("got %v, want certify", c.Subject.Organization[0])
+		}
+
+		if !c.IsCA {
+			t.Fatal("IsCA must be true")
+		}
+
+		if c.DNSNames[0] != "github.com" {
+			t.Fatalf("got %v, want github.com", c.DNSNames[0])
+		}
+
+		if c.IPAddresses[0].String() != "127.0.0.1" {
+			t.Fatalf("got %v, want 127.0.0.1", c.IPAddresses[0].String())
+		}
+
+		tomorrow := time.Now().Add(24 * time.Hour)
+
+		if c.NotAfter.Day() != tomorrow.Day() {
+			t.Fatalf("got %v, want %v", c.NotAfter.Day(), tomorrow.Day())
+		}
+
+	})
+
+}
---
+++ key_test.go
@@ -0,0 +1,51 @@
+package certify
+
+import (
+	"reflect"
+	"testing"
+)
+
+var (
+	PKEYDATA = `-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIL66D9bK8UInoN0xfbQ3/usWXWzHSb8cq+e2RfO6usYpoAoGCCqGSM49
+AwEHoUQDQgAE8aYzn9wIIS+K4/lX6aoUQR18rGLsjKmQgIa+vtc3jwWclNJhh3tT
+AsroDcdnN6haaQt3fmI56TphJF5PXCAhIQ==
+-----END EC PRIVATE KEY-----
+`
+)
+
+func TestGetPrivateKey(t *testing.T) {
+	p, err := GetPrivateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Test created private key can be parsed", func(t *testing.T) {
+		_, err := ParsePrivateKey([]byte(p.String()))
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("Test get public key from created private key", func(t *testing.T) {
+		_, err := GetPublicKey([]byte(p.String()))
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+func TestParsePrivateKey(t *testing.T) {
+	p, err := ParsePrivateKey([]byte(PKEYDATA))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pkey := &PrivateKey{p}
+
+	t.Run("Test compare parsed private key", func(t *testing.T) {
+		if !reflect.DeepEqual(pkey.String(), PKEYDATA) {
+			t.Fatalf("\ngot %v\nwant %v\n", pkey.String(), PKEYDATA)
+		}
+	})
+}
