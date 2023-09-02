2bc8d9943487fe18c90f70eb18f10a5ec94f8a48
nothinux <nothinux@gmail.com>
2023-09-02 21:23:05 +0700

    feat: add crl support

--- cmd/certify/command_test.go
+++ cmd/certify/command_test.go
@@ -247,6 +247,7 @@ func TestCreateCertificate(t *testing.T) {
 	t.Cleanup(func() {
 		os.Remove(caPath)
 		os.Remove(caKeyPath)
+		os.Remove(caCRLPath)
 		os.Remove("nothinux.local.pem")
 		os.Remove("nothinux.local-key.pem")
 	})
@@ -266,6 +267,7 @@ func TestCreateIntermediateCertificate(t *testing.T) {
 	t.Cleanup(func() {
 		os.Remove(caPath)
 		os.Remove(caKeyPath)
+		os.Remove(caCRLPath)
 		os.Remove(caInterPath)
 		os.Remove(caInterKeyPath)
 	})
